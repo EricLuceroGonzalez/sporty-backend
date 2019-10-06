f47a69b9dcd01ee84e4bac0b16ffcd8338f0e067
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2019-10-06 17:31:43 -0500

    .

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: ' ',
+    mongoURI: 'mongodb+srv://ericlucerogonzalez:ericlucerogonzalezmongoDB2019+@cluster0-j4waz.mongodb.net/soccer_db?retryWrites=true&w=majority',
     secretOrKey: "secret"
   };
