fc9b7e7b5432dd82dc903694165fa9cd21b10292
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2019-10-06 19:36:09 -0500

    get methods Co-authored-by: Hefziben <hefziben@gmail.com>

--- server/index.js
+++ server/index.js
@@ -33,71 +33,89 @@ app.get("/", (req, res) => {
 });
 
 //  C: CREATE ------------
+app.post("/api/deporte", (req, res) => {
+  // Recibir el jugador
+  console.log(req.body);
+
+  // Guardar en db
+  const newDeporte = new Deporte(req.body);
+  newDeporte.save((err, newDeporte) => {
+    return err
+      ? res.status(400).send({ mensaje: "Hay un error", res: err })
+      : res.status(200).send({ mensaje: "Deporte guardado", res: newDeporte });
+  });
+});
+
+app.post("/api/liga", (req, res) => {
+  // Recibir el jugador
+  console.log(req.body);
+
+  // Guardar en db
+  const newLeague = new Liga(req.body);
+  newLeague.save((err, newLeague) => {
+    return err
+      ? res.status(400).send({ mensaje: "Hay un error", res: err })
+      : res.status(200).send({ mensaje: "Liga creada", res: newLeague });
+  });
+});
+
+//crear un Equipo dentro de una liga
+app.post('/api/equipo', (req, res)=>{
+  const nuevaEquipo = req.body;
+  const equipoNuevo = new Equipo(nuevaEquipo);
+  equipoNuevo.save((err, equipoNuevo)=>{
+      return err
+      ? res.status(400).send({mensaje: 'error en el post', res:equipoNuevo, err}):
+      res.status(201).send({message: 'Equipo creado', res: equipoNuevo});
+  });
+  console.log(equipoNuevo);
+})
+//obtener todos los deportes
+app.get("/api/deportes", (req, res)=>{
+  Deporte.find()
+           .exec()
+           .then(data =>
+              res.status(200).send(data))
+              .catch(err => res.status(400).send(err));  
+              
+              
+});
+
+//get all Ligas
+app.get("/api/liga", function(req, res) {
+	Liga.find({}, function(err, liga) {
+    	Deporte.populate(liga, {path: "deporte"},function(err, liga){
+        	res.status(200).send(liga);
+        }); 
+    });
+});
+
+app.post("/api/player", (req, res) => {
+  // Recibir el jugador
+  console.log(req.body);
+
+  // Guardar en db
+  const newPlayer = new Player(req.body);
+  newPlayer.save((err, newPlayer) => {
+    return err
+      ? res.status(400).send({ mensaje: "Hay un error", res: err })
+      : res.status(200).send({ mensaje: "Player guardado", res: newPlayer });
+  });
+});
+
 //  C: CREATE ------------
-// app.post("/api/deporte", (req, res) => {
-//   // Recibir el jugador
-//   console.log(req.body);
+app.post("/api/team", (req, res) => {
+  // Recibir el jugador
+  console.log(req.body);
 
-//   // Guardar en db
-//   const newDeporte = new Deporte(req.body);
-//   newDeporte.save((err, newDeporte) => {
-//     return err
-//       ? res.status(400).send({ mensaje: "Hay un error", res: err })
-//       : res.status(200).send({ mensaje: "Deporte guardado", res: newDeporte });
-//   });
-// });
-
-// app.post("/api/liga", (req, res) => {
-//   // Recibir el jugador
-//   console.log(req.body);
-
-//   // Guardar en db
-//   const newLeague = new Liga(req.body);
-//   newLeague.save((err, newLeague) => {
-//     return err
-//       ? res.status(400).send({ mensaje: "Hay un error", res: err })
-//       : res.status(200).send({ mensaje: "Liga creada", res: newLeague });
-//   });
-// });
-
-// //crear un Equipo dentro de una liga
-// app.post('/api/v1/equipo', (req, res)=>{
-//   const nuevaEquipo = req.body;
-//   const equipoNuevo = new Equipo(nuevaEquipo);
-//   equipoNuevo.save((err, equipoNuevo)=>{
-//       return err
-//       ? res.status(400).send({mensaje: 'error en el post', res:equipoNuevo, err}):
-//       res.status(201).send({message: 'Equipo creado', res: equipoNuevo});
-//   });
-//   console.log(equipoNuevo);
-// })
-
-// app.post("/api/player", (req, res) => {
-//   // Recibir el jugador
-//   console.log(req.body);
-
-//   // Guardar en db
-//   const newPlayer = new Player(req.body);
-//   newPlayer.save((err, newPlayer) => {
-//     return err
-//       ? res.status(400).send({ mensaje: "Hay un error", res: err })
-//       : res.status(200).send({ mensaje: "Player guardado", res: newPlayer });
-//   });
-// });
-
-// //  C: CREATE ------------
-// app.post("/api/team", (req, res) => {
-//   // Recibir el jugador
-//   console.log(req.body);
-
-//   // Guardar en db
-//   const newTeam = new Team(req.body);
-//   newTeam.save((err, newTeam) => {
-//     return err
-//       ? res.status(400).send({ mensaje: "Hay un error", res: err })
-//       : res.status(200).send({ mensaje: "Player guardado", res: newTeam });
-//   });
-// });
+  // Guardar en db
+  const newTeam = new Team(req.body);
+  newTeam.save((err, newTeam) => {
+    return err
+      ? res.status(400).send({ mensaje: "Hay un error", res: err })
+      : res.status(200).send({ mensaje: "Player guardado", res: newTeam });
+  });
+});
 
 // Passport middleware
 // app.use(passport.initialize()); // Passport config
