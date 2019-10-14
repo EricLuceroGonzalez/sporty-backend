a405f8c1eb3c964ee6242a6991d0477c2144e2a0
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2019-10-14 18:16:22 -0500

    then and catch

--- server/index.js
+++ server/index.js
@@ -105,7 +105,9 @@ app.get("/api/ligaDetail/:id", function(req, res) {
     Liga.populate(user, { path: "deporte" }, function(err, user) {
       console.log(user);
     });
-  });
+  })
+  .then(liga => res.status(200).send({ mensaje: "Ligas!!", res: liga }))
+  .catch(err => res.status(400).send({ mensaje: "Hay un error", res: err }));
 });
 
 app.post("/api/player", (req, res) => {
