c7aed5499d4942df46a2934d241e25ba2eb59427
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2019-10-13 21:40:42 -0500

    get liga by id comment

--- server/index.js
+++ server/index.js
@@ -87,18 +87,18 @@ app.get("/api/liga", function(req, res) {
     Deporte.populate(liga, { path: "deporte" }, function(err, liga) {
       return err
         ? res.status(400).send({ mensaje: "Hay un error", res: err })
-        : res.status(200).sen({ mensaje: "Ligas!!", res: liga });
+        : res.status(200).send({ mensaje: "Ligas!!", res: liga });
     });
   });
 });
 
-// // Get one liga by ID
-// app.get("/api/ligaDetail/:id", function(req, res) {
-//   Liga.findById(req.params.id)
-//     .exec()
-//     .then(liga => res.status(200).send({ mensaje: "Ligas!!", res: liga }))
-//     .catch(err => res.status(400).send({ mensaje: "Hay un error", res: err }));
-// });
+// Get one liga by ID
+app.get("/api/ligaDetail/:id", function(req, res) {
+  Liga.findById(req.params.id)
+    .exec()
+    .then(liga => res.status(200).send({ mensaje: "Ligas!!", res: liga }))
+    .catch(err => res.status(400).send({ mensaje: "Hay un error", res: err }));
+});
 
 app.post("/api/player", (req, res) => {
   // Recibir el jugador
